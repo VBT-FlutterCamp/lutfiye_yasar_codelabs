a5156033dd4b74d2cd66bdfb035f9d3a0c69567e
Ltfyysr <lutfiyeyasar38@gmail.com>
2022-02-15 12:38:20 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:vbt_camp_homework1/words.dart';
-// Add this line.
 
 void main() => runApp(MyApp());
 
